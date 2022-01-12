edc7658890961aaaded9fdc015361156b87e68c7
wl4dig3 <wladige@gmail.com>
2022-01-12 10:23:46 -0300

    agregando material design icon y fontawesome

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons:{
+        iconfont:'md' || 'fa'
+    }
 });
--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Contacto.vue')
+    component: () => import(/* webpackChunkName: "contacto" */ '../views/Contacto.vue')
   },
   {
   path: '/login',
@@ -32,7 +32,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   },
 ]
 

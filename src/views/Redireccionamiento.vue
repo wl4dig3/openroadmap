b19c98182dea98f3938cfe911fdbd724ea34e9e1
wl4dig3 <wladige@gmail.com>
2022-01-12 10:51:27 -0300

    actualizando funcionalidad login y redirigir a una pagina nueva

---
+++ src/views/Redireccionamiento.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-main>
+      <div class="text-center">
+        <v-btn class="mx-2" fab color="black" outlined>
+          <v-icon class="material-icons" @click="logout">logout</v-icon>
+        </v-btn>
+      </div>
+      <h1 class="pag text-center">Pagina de redireccionamiento</h1>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      //.then(() => this.$router.replace("login"));
+      alert("estas fuera");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pag {
+  margin-top: 50px;
+}
+</style>
